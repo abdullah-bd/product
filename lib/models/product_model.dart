c5926b99caf5323b558caab85b98a3f0b3e6a0c3
Abdullah-bd <Abdullah-bd>
2022-02-21 23:12:23 +0600

    Date picker added.

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/cupertino.dart';
 
 class Product{
-  int id;
-  String name;
-  String category;
-  String description;
-  double price;
-  bool isFavourite;
-  String uploadDate;
-  double rating;
-  String image;
+  int? id;
+  String? name;
+  String? category;
+  String? description;
+  double? price;
+  bool? isFavourite;
+  String? uploadDate;
+  double rating = 0.0;
+  String? image;
 
-  Product(this.id,this.name, this.category, this.description, this.price,
-      this.isFavourite, this.uploadDate, this.rating, this.image);
+  Product();
+
+  @override
+  String toString() {
+    return 'Product{id: $id, name: $name, category: $category, description: $description, price: $price, isFavourite: $isFavourite, uploadDate: $uploadDate, rating: $rating, image: $image}';
+  }
 }
--- lib/pages/new_product_page.dart
+++ lib/pages/new_product_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:product_app/models/product_model.dart';
 
 class NewProductPage extends StatefulWidget {
   const NewProductPage({Key? key}) : super(key: key);
@@ -8,6 +9,21 @@ class NewProductPage extends StatefulWidget {
 }
 
 class _NewProductPageState extends State<NewProductPage> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  var product = Product();
+
+  _saveProduct(){
+
+    if(_formKey.currentState!.validate()){
+      _formKey.currentState!.save();
+      print(product);
+    }
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,8 +33,10 @@ class _NewProductPageState extends State<NewProductPage> {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Form(
+          key: _formKey,
           child: ListView(
             children: [
+              SizedBox(height: 10,),
               TextFormField(
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
@@ -33,6 +51,7 @@ class _NewProductPageState extends State<NewProductPage> {
                 },
                 onSaved: (newValue) {
 
+                  product.name = newValue!;
                 },
               ),
               SizedBox(height: 10,),
@@ -45,16 +64,16 @@ class _NewProductPageState extends State<NewProductPage> {
                 maxLines: 3,
                 validator: (value) {
                   if(value!.isEmpty){
-                    return 'Product Name cannot be Empty!';
+                    return 'Product Description cannot be Empty!';
                   }
                   return null;
                 },
                 onSaved: (newValue) {
 
+                  product.description = newValue!;
                 },
               ),
               SizedBox(height: 10,),
-
               TextFormField(
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
@@ -73,14 +92,39 @@ class _NewProductPageState extends State<NewProductPage> {
                 },
                 onSaved: (newValue) {
 
+                  product.price = double.parse(newValue!);
+
+                  product.id = 1;
                 },
               ),
-              SizedBox(height: 10,),
-              RaisedButton(
-                textColor: Colors.white,
-                color: Theme.of(context).primaryColor,
-                onPressed: _saveProduct ,
-                child: Text("Save" ),
+              SizedBox(height: 20,),
+              Text('Select Purchase Date'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text('No Date Chosen'),
+                    FlatButton(onPressed: (){
+                      Future<DateTime?> fdt = showDatePicker(context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2022),
+                          lastDate: DateTime.now()
+                      );
+                      fdt.then((value) => print(value!.toIso8601String()));
+                    }, child: Text('Select Date'))
+
+                  ],
+                ),
+              ),
+              Center(
+                child: RaisedButton(
+                  textColor: Colors.white,
+                  shape: RoundedRectangleBorder (borderRadius: BorderRadius.circular(8)),
+                  color: Theme.of(context).primaryColor,
+                  onPressed: _saveProduct ,
+                  child: Text("Save" ),
+                ),
               )
             ],
 
@@ -90,6 +134,4 @@ class _NewProductPageState extends State<NewProductPage> {
     ) ;
   }
 
-  _saveProduct() {
-  }
 }
